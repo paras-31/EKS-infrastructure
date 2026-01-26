6f216c17b33358cfcef4c1f4caa8cb7f22fe3b7b
paras-MCK <paras_kamboj@external.mckinsey.com>
2026-01-26 11:58:57 +0530

    pushed with updated credentials

--- eks/main.tf
+++ eks/main.tf
@@ -27,19 +27,3 @@ resource "aws_eks_node_group" "nodes" {
 }
 
 
-
-resource "aws_eks_access_entry" "admin" {
-  cluster_name = aws_eks_cluster.eks.name
-  principal_arn = "arn:aws:iam::000087384605:role/AWSReservedSSO_AVM-AdministratorAccess-d97965"
-  type = "STANDARD"
-}
-
-resource "aws_eks_access_policy_association" "admin_policy" {
-  cluster_name = aws_eks_cluster.eks.name
-  principal_arn = aws_eks_access_entry.admin.principal_arn
-  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-
-  access_scope {
-    type = "cluster"
-  }
-}
