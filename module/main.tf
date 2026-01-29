c67c657a286032df29b4e7439be0fd87e268f3ba
paras-MCK <paras_kamboj@external.mckinsey.com>
2026-01-29 16:08:23 +0530

    data source updated

--- module/main.tf
+++ module/main.tf
@@ -2,25 +2,40 @@ module "vpc" {
   source = "../vpc"
 }
 
+data "aws_iam_roles" "eks_cluster_role_matches" {
+  name_regex = "^eks-cluster-role$"
+}
+
+data "aws_iam_roles" "node_role_matches" {
+  name_regex = "^eks-node-role$"
+}
+
+locals {
+  eks_cluster_role_exists = length(data.aws_iam_roles.eks_cluster_role_matches.arns) > 0
+  eks_node_role_exists    = length(data.aws_iam_roles.node_role_matches.arns) > 0
+}
+
 data "aws_iam_role" "eks_cluster_role_existing" {
-  name = "eks-cluster-role"
+  count = local.eks_cluster_role_exists ? 1 : 0
+  name  = "eks-cluster-role"
 }
 
 data "aws_iam_role" "node_role_existing" {
-  name = "eks-node-role"
+  count = local.eks_node_role_exists ? 1 : 0
+  name  = "eks-node-role"
 }
 
 
 module "iam" {
-  count = try(data.aws_iam_role.eks_cluster_role_existing.name, "") == "" ? 1 : 0
+  count  = local.eks_cluster_role_exists && local.eks_node_role_exists ? 0 : 1
   source = "../iam"
 }
 
 module "eks" {
   source            = "../eks"
   cluster_name      = "eksdemo"
-  role_arn          = try(data.aws_iam_role.eks_cluster_role_existing.arn, module.iam.eks_cluster_role)
-  node_role_arn     = try(data.aws_iam_role.node_role_existing.arn, module.iam.eks_node_role)
+  role_arn          = local.eks_cluster_role_exists ? data.aws_iam_role.eks_cluster_role_existing[0].arn : module.iam.eks_cluster_role
+  node_role_arn     = local.eks_node_role_exists ? data.aws_iam_role.node_role_existing[0].arn : module.iam.eks_node_role
   public_subnet_ids = module.vpc.public_subnet_ids
   private_subnet_ids= module.vpc.private_subnet_ids
   depends_on = [ module.iam,module.vpc ]
